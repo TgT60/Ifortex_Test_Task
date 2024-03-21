2d86444335d7d9262ce66ea2a886878af534bfb5
TgT60 <dima.icigo@yandex.ru>
2024-03-21 16:06:23 +0300

    Created Test Task

---
+++ TestTask/TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrderService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return Task.FromResult(_dbContext.Orders
+                .OrderByDescending(order => order.Price * order.Quantity)
+                .FirstOrDefault());
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _dbContext.Orders.Where(order => order.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<User> GetUser()
+        {
+            return Task.FromResult(_dbContext.Users
+                .OrderByDescending(u => u.Orders.Count)
+                .FirstOrDefault());
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _dbContext.Users.Where(user => user.Status == UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
